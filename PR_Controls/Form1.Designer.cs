d8f500c6ecb41a2e12d2dd4a972ecb1b6e6a55e4
Makinator0 <maksimafom@gmail.com>
2024-05-22 21:58:03 +0300

    Add project files.

---
+++ PR_Controls/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PR_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.цветТекстаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шрифтToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.работаСДатамиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.часыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.расчетОстаткаВремениToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ContextMenuStrip = this.contextMenuStrip1;
+            this.label1.Location = new System.Drawing.Point(22, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1 ";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.цветТекстаToolStripMenuItem,
+            this.шрифтToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(138, 48);
+            // 
+            // цветТекстаToolStripMenuItem
+            // 
+            this.цветТекстаToolStripMenuItem.Name = "цветТекстаToolStripMenuItem";
+            this.цветТекстаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.цветТекстаToolStripMenuItem.Text = "Цвет текста";
+            this.цветТекстаToolStripMenuItem.Click += new System.EventHandler(this.цветТекстаToolStripMenuItem_Click);
+            // 
+            // шрифтToolStripMenuItem
+            // 
+            this.шрифтToolStripMenuItem.Name = "шрифтToolStripMenuItem";
+            this.шрифтToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.шрифтToolStripMenuItem.Text = "Шрифт";
+            this.шрифтToolStripMenuItem.Click += new System.EventHandler(this.шрифтToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.работаСДатамиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // работаСДатамиToolStripMenuItem
+            // 
+            this.работаСДатамиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.часыToolStripMenuItem,
+            this.расчетОстаткаВремениToolStripMenuItem});
+            this.работаСДатамиToolStripMenuItem.Name = "работаСДатамиToolStripMenuItem";
+            this.работаСДатамиToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.работаСДатамиToolStripMenuItem.Text = "Работа с датами";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // часыToolStripMenuItem
+            // 
+            this.часыToolStripMenuItem.Name = "часыToolStripMenuItem";
+            this.часыToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.часыToolStripMenuItem.Text = "Часы";
+            this.часыToolStripMenuItem.Click += new System.EventHandler(this.часыToolStripMenuItem_Click);
+            // 
+            // расчетОстаткаВремениToolStripMenuItem
+            // 
+            this.расчетОстаткаВремениToolStripMenuItem.Name = "расчетОстаткаВремениToolStripMenuItem";
+            this.расчетОстаткаВремениToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.расчетОстаткаВремениToolStripMenuItem.Text = "Расчет остатка времени";
+            this.расчетОстаткаВремениToolStripMenuItem.Click += new System.EventHandler(this.расчетОстаткаВремениToolStripMenuItem_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(28, 204);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem цветТекстаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шрифтToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem работаСДатамиToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem часыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem расчетОстаткаВремениToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ PR_Controls/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR_Controls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+        private void InitializeCustomComponents()
+        {
+            TabControl tabControl1 = new TabControl();
+            tabControl1.Size = new Size(400, 300);
+            tabControl1.Location = new Point(this.ClientSize.Width - tabControl1.Width - 10, 10);
+            tabControl1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            TabPage tabPage1 = new TabPage("Лекции");
+            TabPage tabPage2 = new TabPage("Изображение");
+
+
+            CheckedListBox checkedListBox1 = new CheckedListBox();
+            checkedListBox1.Items.AddRange(new object[] {
+                "Лекция 1",
+                "Лекция 2",
+                "Лекция 3",
+                "Лекция 4"
+            });
+            checkedListBox1.Location = new Point(10, 10);
+
+            Button buttonShowSelected = new Button();
+            buttonShowSelected.Text = "Показать выбранные лекции";
+            buttonShowSelected.Location = new Point(10, 120);
+            buttonShowSelected.Click += ButtonShowSelected_Click;
+
+            tabPage1.Controls.Add(checkedListBox1);
+            tabPage1.Controls.Add(buttonShowSelected);
+
+            StatusStrip statusStrip1 = new StatusStrip();
+            statusStrip1.Name = "statusStrip1";
+            ToolStripStatusLabel toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1.Items.Add(toolStripStatusLabel1);
+            statusStrip1.Location = new Point(0, this.ClientSize.Height - statusStrip1.Height);
+            statusStrip1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            Button buttonOpenFileDialog = new Button();
+            buttonOpenFileDialog.Text = "Выбрать изображение";
+            buttonOpenFileDialog.Location = new Point(10, 10);
+            buttonOpenFileDialog.Click += ButtonOpenFileDialog_Click;
+
+            PictureBox pictureBox1 = new PictureBox();
+            pictureBox1.Size = new Size(300, 200);
+            pictureBox1.Location = new Point(10, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            tabPage2.Controls.Add(buttonOpenFileDialog);
+            tabPage2.Controls.Add(pictureBox1);
+
+            tabControl1.TabPages.Add(tabPage1);
+            tabControl1.TabPages.Add(tabPage2);
+
+            this.Controls.Add(tabControl1);
+            this.Controls.Add(statusStrip1);
+        }
+
+        private void ButtonShowSelected_Click(object sender, EventArgs e)
+        {
+            TabPage tabPage = (TabPage)((Button)sender).Parent;
+            CheckedListBox checkedListBox1 = (CheckedListBox)tabPage.Controls[0];
+
+            StatusStrip statusStrip1 = (StatusStrip)this.Controls["statusStrip1"];
+            ToolStripStatusLabel toolStripStatusLabel1 = (ToolStripStatusLabel)statusStrip1.Items[0];
+
+            toolStripStatusLabel1.Text = "Выбранные лекции: " + string.Join(", ", checkedListBox1.CheckedItems.OfType<string>().ToArray());
+        }
+
+        private void ButtonOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TabPage tabPage = (TabPage)((Button)sender).Parent;
+                PictureBox pictureBox1 = (PictureBox)tabPage.Controls[1];
+
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void цветТекстаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            label1.ForeColor = colorDialog1.Color;
+        }
+
+        private void шрифтToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            label1.Font = fontDialog1.Font;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void часыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += delegate { label2.Text = DateTime.Now.ToLongTimeString() + "далее время по частям:" + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.ToString(); };
+        }
+
+        private void расчетОстаткаВремениToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            DateTime dt1 = dateTimePicker1.Value;
+            label2.Text = "До даты " + dt1.ToLongDateString() + " осталось " + (dt1 - DateTime.Now).Days + " дней";
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ListView lv1 = new ListView();
+            lv1.Name = "lv1";
+            lv1.Size = new Size(360, 160);
+            lv1.Location = new Point(50, 200);
+            lv1.View = View.Details;
+            lv1.Columns.Add("тип элемента", 240);
+            lv1.Columns.Add("имя элемента", 120);
+            Controls.Add(lv1);
+            foreach (Control c1 in Controls)
+            {
+                string[] s2 = new string[2];
+                s2[0] = c1.GetType().ToString();
+                s2[1] = c1.Name;
+                ListViewItem lvi1 = new ListViewItem(s2);
+                lv1.Items.Add(lvi1);
+            }
+        }
+    }
+}
